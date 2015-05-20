734cd0101a7d6da38166643050ff4da7dba982cb
William Wong <william.wong@candrholdings.com>
2015-05-21 02:10:55 +0800

    Add test for options.cache

---
+++ tools/test/publish-cache-test.js
@@ -0,0 +1,53 @@
+!function (assert, publish, FileSystemCache) {
+    'use strict';
+
+    require('vows').describe('Cache options in "publish" module').addBatch({
+        'When passing null': {
+            topic: publish({ cache: null }).options.cache,
+            'should cache by FileSystemCache': function (topic) {
+                assert(topic instanceof FileSystemCache);
+            },
+            'should cache with default settings': function (topic) {
+                assert(!topic.options.tempdir);
+            }
+        },
+
+        'When passing false': {
+            topic: publish({ cache: false }).options.cache,
+            'should not returns any cache': function (topic) {
+                assert(!topic);
+            }
+        },
+
+        'When passing a string': {
+            topic: publish({ cache: 'path/' }).options.cache,
+            'should cache by FileSystemCache': function (topic) {
+                assert(topic instanceof FileSystemCache);
+            },
+            'should cache at specified path': function (topic) {
+                assert.equal(topic.options.tempdir, 'path/');
+            }
+        },
+
+        'When passing a new object': {
+            topic: function () {
+                var callback = this.callback;
+
+                try {
+                    publish({ cache: {} }).options.cache
+                } catch (ex) {
+                    return callback(null, ex);
+                }
+
+                callback(new Error('exception not thrown'));
+            },
+            'should throws exception': function (topic) {
+                assert(topic);
+            }
+        }
+    }).export(module);
+}(
+    require('assert'),
+    require('../publish'),
+    require('../filesystemcache')
+);
