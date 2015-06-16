80c7eab31c18ae7f60b48d0852d7a88516058862
compulim <compulim@hotmail.com>
2015-06-16 14:16:08 +0800

    Save processor will purge deleted files

--- src/processors/save.js
+++ src/processors/save.js
@@ -1,70 +1,97 @@
 !function (async, fs, linq, mkdirp, number, path, time) {
     'use strict';
 
-    module.exports = function (inputs, outputs, dirpath, callback) {
+    module.exports = function (inputs, outputs, dirpath, options, callback) {
         var that = this,
-            options = that.options,
+            outputdir = that.options.output,
             isDir = /\/$/.test(dirpath),
             startTime = Date.now(),
             totalSize = 0;
+
+        if (arguments.length < 5) {
+            callback = arguments[arguments.length - 1];
+        }
+
+        if (arguments.length === 3) {
+            dirpath = '.';
+            options = null;
+        } else if (arguments.length === 4) {
+            if (typeof dirpath === 'string') {
+                options = null;
+            } else {
+                options = arguments[2];
+                dirpath = '.';
+            }
+        }
+
+        options || (options = {});
+        options.purge = options.purge !== false;
 
         if (!isDir && linq(outputs).count().run() > 1) {
             return callback(new Error('Cannot save multiple outputs to a single file, consider append / to the output path'));
         }
 
         // Delete orphaned files from output
-        inputs.deleted.forEach(function (filename) {
-            var outputFilename = isDir ? path.join(dirpath, filename).replace(/\\/g, '/') : dirpath;
+        async.parallel([
+            function (callback) {
+                linq(inputs.deleted).async.select(function (filename, _, callback) {
+                    var outputFilename = isDir ? path.join(dirpath, filename).replace(/\\/g, '/') : dirpath;
 
-            outputs[outputFilename] = null;
-        });
+                    fs.unlink(path.resolve(outputdir, outputFilename), function (err) {
+                        if (!err || err.code === 'ENOENT') {
+                            outputs[outputFilename] = null;
+                        }
 
-        linq(inputs.newOrChanged).async.select(function (entry, filename, callback) {
-            var outputFilename = isDir ? path.join(dirpath, filename).replace(/\\/g, '/') : dirpath,
-                fullname = path.resolve(options.output, outputFilename);
+                        callback(err);
+                    });
+                }, null).run(callback);
+            },
+            function (callback) {
+                linq(inputs.newOrChanged).async.select(function (entry, filename, callback) {
+                    var outputFilename = isDir ? path.join(dirpath, filename).replace(/\\/g, '/') : dirpath,
+                        fullname = path.resolve(outputdir, outputFilename);
 
-            outputs[outputFilename] = entry;
+                    outputs[outputFilename] = entry;
+                    totalSize += entry.length;
 
-            mkdirp(path.dirname(fullname), function (err) {
-                if (err) { return callback(err); }
+                    mkdirp(path.dirname(fullname), function (err) {
+                        err ? callback(err) : fs.writeFile(fullname, entry, callback);
+                    });
+                }, null).run(callback);
+            }
+        ], function (err) {
+            if (err) { return callback(err); }
 
-                fs.writeFile(fullname, entry, callback);
-            });
+            var numAll = Object.getOwnPropertyNames(inputs.all).length,
+                numNewOrChanged = Object.getOwnPropertyNames(inputs.newOrChanged).length,
+                displayPath = path.relative(process.cwd(), dirpath).replace(/\\/g, '/'),
+                displayableFiles = linq(inputs.newOrChanged).toArray(function (_, filename) { return filename; }).orderBy().take(5).run(),
+                elapsed = Date.now() - startTime;
 
-            totalSize += entry.length;
-        }).run(function (err) {
-            if (!err) {
-                var numAll = Object.getOwnPropertyNames(inputs.all).length,
-                    numNewOrChanged = Object.getOwnPropertyNames(inputs.newOrChanged).length,
-                    displayPath = path.relative(process.cwd(), dirpath).replace(/\\/g, '/'),
-                    displayableFiles = linq(inputs.newOrChanged).toArray(function (_, filename) { return filename; }).orderBy().take(5).run(),
-                    elapsed = Date.now() - startTime;
-
-                if (numAll && !numNewOrChanged) {
-                    that.log('No new changes to save to ./' + displayPath + ' because all ' + numAll + ' file(s) were unchanged');
-                } else if (!numAll) {
-                    that.log('No files to save to ./' + displayPath);
-                } else {
-                    that.log([
-                        'Saving ',
-                        numNewOrChanged,
-                        ' new or changed file(s) to ./',
-                        displayPath,
-                        ', including ',
-                        displayableFiles.join(', '),
-                        displayableFiles.length !== numAll ? '\u2026' : '',
-                        ', took ',
-                        time.humanize(elapsed),
-                        ' (',
-                        number.bytes(totalSize / elapsed * 1000),
-                        '/s, total ',
-                        number.bytes(totalSize),
-                        ')'
-                    ].join(''));
-                }
+            if (numAll && !numNewOrChanged) {
+                that.log('No new changes to save to ./' + displayPath + ' because all ' + numAll + ' file(s) were unchanged');
+            } else if (!numAll) {
+                that.log('No files to save to ./' + displayPath);
+            } else {
+                that.log([
+                    'Saving ',
+                    numNewOrChanged,
+                    ' new or changed file(s) to ./',
+                    displayPath,
+                    ', including ',
+                    displayableFiles.join(', '),
+                    displayableFiles.length !== numAll ? '\u2026' : '',
+                    ', took ',
+                    time.humanize(elapsed),
+                    ' (',
+                    number.bytes(totalSize / elapsed * 1000),
+                    '/s, total ',
+                    number.bytes(totalSize),
+                    ')'
+                ].join(''));
             }
 
-            callback(err, err ? null : outputs);
+            callback(null, outputs);
         });
     };
 }(
