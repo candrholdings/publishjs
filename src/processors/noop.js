49d5b85184b4fe832e3482d5fbdccd1c15604588
compulim <compulim@hotmail.com>
2015-06-16 21:56:17 +0800

    Noop processor should output all input files

--- src/processors/noop.js
+++ src/processors/noop.js
@@ -2,10 +2,6 @@
     'use strict';
 
     module.exports = function (inputs, outputs, callback) {
-        Object.getOwnPropertyNames(inputs.newOrChanged).forEach(function (filename) {
-            outputs[filename] = inputs.newOrChanged[filename];
-        });
-
-        callback(null, outputs);
+        callback(null, inputs.all);
     };
 }();
