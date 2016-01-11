6ec8c681445392d9e021ce8da907223f652241fa
compulim <compulim@hotmail.com>
2016-01-11 18:10:52 +0800

    Fix no minify build with processor options

--- src/processors/noop.js
+++ src/processors/noop.js
@@ -1,7 +1,7 @@
 !function () {
     'use strict';
 
-    module.exports = function (inputs, outputs, callback) {
-        callback(null, inputs.all);
+    module.exports = function (inputs) {
+        arguments[arguments.length - 1](null, inputs.all);
     };
 }();
